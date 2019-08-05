501ed1e16f9dd9e180da7eaebb85cb1061916b25
JimGerth <jim.frederic.gerth@gmail.com>
2019-08-05 14:06:52 +0200

    added html_statement() method and respective test

--- Customer.py
+++ Customer.py
@@ -15,13 +15,25 @@ class Customer:
     def statement(self):
         result = f"Rental Record for {self.get_name()}\n"
         for rental in self._rentals:
-            
+
             # show figures for this rental
             result += f"\t{rental.get_movie().get_title()}\t{rental.get_charge()}\n"
 
         # add footer lines
         result += f"Amount owed is {self.get_total_charge()}\n"
         result += f"You earned {self.get_total_frequent_renter_points()} frequent renter points"
+        return result
+
+    def html_statement(self):
+        result = f"<h1>Rentals for <em>{self.get_name()}</em></h1><p>\n"
+        for rental in self._rentals:
+
+            # show figures for this rental
+            result += f"{rental.get_movie().get_title()}: {rental.get_charge()}<br>\n"
+
+        # add footer lines
+        result += f"<p>You owe <em>{self.get_total_charge()}</em></p>\n"
+        result += f"On this rental you earned <em>{self.get_total_frequent_renter_points()}</em> frequent renter points<p>"
         return result
 
     def get_total_charge(self):
--- CustomerTest.py
+++ CustomerTest.py
@@ -15,3 +15,10 @@ class CustomerTest(unittest.TestCase):
         tom.add_rental(Rental(Movie("Inception", Movie.REGULAR), 3))
         tom.add_rental(Rental(Movie("The Shining", Movie.REGULAR), 9))
         self.assertEqual(tom.statement(), "Rental Record for Tom\n\tShutter Island\t6.5\n\tInception\t3.5\n\tThe Shining\t12.5\nAmount owed is 22.5\nYou earned 3 frequent renter points")
+
+    def test_statement(self):
+        tom = Customer("Tom")
+        tom.add_rental(Rental(Movie("Shutter Island", Movie.REGULAR), 5))
+        tom.add_rental(Rental(Movie("Inception", Movie.REGULAR), 3))
+        tom.add_rental(Rental(Movie("The Shining", Movie.REGULAR), 9))
+        self.assertEqual(tom.html_statement(), "<h1>Rentals for <em>Tom</em></h1><p>\nShutter Island: 6.5<br>\nInception: 3.5<br>\nThe Shining: 12.5<br>\n<p>You owe <em>22.5</em></p>\nOn this rental you earned <em>3</em> frequent renter points<p>")
