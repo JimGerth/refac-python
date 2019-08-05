685190c3c8cae991942b75ceb153e93178b00585
JimGerth <jim.frederic.gerth@gmail.com>
2019-08-05 12:51:01 +0200

    refactored lengthy if statements into its own method and moved that into a more appropriate class - Rental

--- Customer.py
+++ Customer.py
@@ -17,21 +17,8 @@ class Customer:
         frequent_renter_points = 0
         result = f"Rental Recor for {self.get_name()}\n"
         for rental in self._rentals:
-            this_amount = 0.0
 
-            # determine amounts for each line
-            if rental.get_movie().get_price_code() == Movie.REGULAR:
-                this_amount += 2
-                if rental.get_days_rented() > 2:
-                    this_amount += (rental.get_days_rented() - 2) * 1.5
-
-            elif rental.get_movie().get_price_code() == Movie.NEW_RELEASE:
-                this_amount += rental.get_days_rented() * 3
-
-            elif rental.get_movie().get_price_code() == Movie.CHILDRENS:
-                this_amount += 1.5
-                if rental.get_days_rented() > 3:
-                    this_amount += (rental.get_days_rented() - 3) * 1.5
+            this_amount = rental.get_charge()
 
             # add frequent renter points
             frequent_renter_points += 1
--- Rental.py
+++ Rental.py
@@ -1,3 +1,4 @@
+from Movie import Movie
 
 class Rental:
 
@@ -10,3 +11,20 @@ class Rental:
 
     def get_movie(self):
         return self._movie
+
+    def get_charge(self):
+        result = 0.0
+        if self.get_movie().get_price_code() == Movie.REGULAR:
+            result += 2
+            if self.get_days_rented() > 2:
+                result += (self.get_days_rented() - 2) * 1.5
+
+        elif self.get_movie().get_price_code() == Movie.NEW_RELEASE:
+            result += self.get_days_rented() * 3
+
+        elif self.get_movie().get_price_code() == Movie.CHILDRENS:
+            result += 1.5
+            if self.get_days_rented() > 3:
+                result += (self.get_days_rented() - 3) * 1.5
+
+        return result
