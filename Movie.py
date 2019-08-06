1c050e34f3cc8728d9fcaa93783d9f216b8f8e50
JimGerth <jim.frederic.gerth@gmail.com>
2019-08-06 11:07:54 +0200

    moved get_charge method into movie class

--- Movie.py
+++ Movie.py
@@ -17,3 +17,20 @@ class Movie:
 
     def get_title(self):
         return self._title
+
+    def get_charge(self, daysRented):
+        result = 0.0
+        if self.get_price_code() == Movie.REGULAR:
+            result += 2
+            if daysRented > 2:
+                result += (daysRented - 2) * 1.5
+
+        elif self.get_price_code() == Movie.NEW_RELEASE:
+            result += daysRented * 3
+
+        elif self.get_price_code() == Movie.CHILDRENS:
+            result += 1.5
+            if daysRented > 3:
+                result += (daysRented - 3) * 1.5
+
+        return result
--- Rental.py
+++ Rental.py
@@ -13,21 +13,7 @@ class Rental:
         return self._movie
 
     def get_charge(self):
-        result = 0.0
-        if self.get_movie().get_price_code() == Movie.REGULAR:
-            result += 2
-            if self.get_days_rented() > 2:
-                result += (self.get_days_rented() - 2) * 1.5
-
-        elif self.get_movie().get_price_code() == Movie.NEW_RELEASE:
-            result += self.get_days_rented() * 3
-
-        elif self.get_movie().get_price_code() == Movie.CHILDRENS:
-            result += 1.5
-            if self.get_days_rented() > 3:
-                result += (self.get_days_rented() - 3) * 1.5
-
-        return result
+        return self._movie.get_charge(self._days_rented)
 
     def get_frequent_renter_points(self):
         if self.get_movie().get_price_code() == Movie.NEW_RELEASE and self.get_days_rented() > 1:
